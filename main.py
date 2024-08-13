40eb998abed7cc455ab9f3b7a017fe7e29ea0434
Mitles1234 <128334837+Mitles1234@users.noreply.github.com>
2024-08-13 14:15:50 +1000

    13/08/24 - Testing and Implimenting
    Created A graph with the data, learnt by removing the graph style part it would default to a line graph which I wanted.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #--- Imports ---
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 #----Global Variables----#
 quit = False
@@ -10,16 +11,32 @@ Weather_df = pd.read_csv('data/Weather.csv')
 
 #--- Albury ---
 Albury_Weather_df = Weather_df[Weather_df.Location == 'Albury']
-Gusts_Albury_Weather_df = Albury_Weather_df.filter('WindGustSpeed')
-WindSpeed_Albury_Weather_df = Albury_Weather_df.filter('WindSpeed3pm')
+
+#--- Gusts ---
+Gusts_Albury_Weather_df = Albury_Weather_df[['Date', 'WindGustSpeed']]
+
+#--- Wind Speed ---
+WindSpeed_Albury_Weather_df = Albury_Weather_df[['Date', 'WindSpeed3pm']]
 
 print(WindSpeed_Albury_Weather_df)
 
-'''WindSpeed_Albury_Weather_df.plot(
-                    kind='bar',
-                    x='Time',
-                    y='Speed',
+'''Gusts_Albury_Weather_df.plot(
+                    #kind='bar',
+                    x='Date',
+                    y= 'WindGustSpeed',
                     color='blue',
                     alpha=0.3,
-                    title='Cost of a Big Mac in AUD')
-plt.show()'''
+                    title='Gust Wind Speed in Albury'
+              )'''
+
+WindSpeed_Albury_Weather_df.plot(
+                    #kind='bar',
+                    x='Date',
+                    y= 'WindSpeed3pm',
+                    color='blue',
+                    alpha=0.3,
+                    title='Wind Speed in Albury'
+              )
+
+plt.show()
+
